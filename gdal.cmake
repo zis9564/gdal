153e11140220960763e5e7521aa87885ec5809b7
Zakharov <ivan.zakharov@emlid.com>
2025-12-03 17:20:32 +0000

    geodata-core: cmake: fix uninstall pkg conflict

--- gdal.cmake
+++ gdal.cmake
@@ -612,8 +612,15 @@ if (NOT GDAL_ENABLE_MACOSX_FRAMEWORK)
     COMPONENT libraries)
 endif ()
 
-configure_file(${GDAL_CMAKE_TEMPLATE_PATH}/uninstall.cmake.in ${PROJECT_BINARY_DIR}/cmake_uninstall.cmake @ONLY)
-add_custom_target(uninstall COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+if(NOT TARGET uninstall)
+    configure_file(
+        "${PROJECT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
+        IMMEDIATE @ONLY)
+
+    add_custom_target(uninstall
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake)
+endif()
 
 ################################################################
 # Final reports and warnings
